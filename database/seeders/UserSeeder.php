af4f6dab3ae0433aa6166224a54a7107470109b0
iban.rama <ibanrama29@gmail.com>
2024-11-24 06:02:55 +0700

    jejak

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::updateOrCreate(
+            ["email" => "super-admin@gmail.com"],
+            [
+                'name' => 'Super Admin',
+                'email' => 'super-admin@gmail.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('admin'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        );
+
+        $admin->assignRole('Super Admin');
+    }
+}
